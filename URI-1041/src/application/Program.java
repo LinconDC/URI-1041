f5a63dff11855eaa61a4a4d7541961b5f652b4c5
linco <linco@DESKTOP-VIJS1C2>
2022-04-08 17:32:38 -0300

    code indentation

--- URI-1041/src/application/Program.java
+++ URI-1041/src/application/Program.java
@@ -6,37 +6,30 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Insira 2 valores com ponto flutuante: ");
 		double numX = sc.nextDouble();
 		double numY = sc.nextDouble();
-		
+
 		if (numX == 0.0 && numY == 0.0) {
 			System.out.println("Origem");
-		}
-		else if (numX == 0.0) {
+		} else if (numX == 0.0) {
 			System.out.println("Eixo Y");
-		}
-		else if (numY == 0.0) {
+		} else if (numY == 0.0) {
 			System.out.println("Eixo X");
-		}
-		else if (numX > 0.0 && numY > 0.0) {
+		} else if (numX > 0.0 && numY > 0.0) {
 			System.out.println("Quadrante 1");
-		}
-		else if (numX < 0.0 && numY > 0.0) {
+		} else if (numX < 0.0 && numY > 0.0) {
 			System.out.println("Quadrante 2");
-		}
-		else if (numX < 0.0 && numY < 0.0) {
+		} else if (numX < 0.0 && numY < 0.0) {
 			System.out.println("Quadrante 3");
-		}
-		else {
+		} else {
 			System.out.println("Quadrante 4");
 		}
-		
-		
+
 		sc.close();
 
 	}
